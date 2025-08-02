bf4c94203fd7d03b09b320631e8f3126088c783e
praveen0309d <d.praveen0309@gmail.com>
2025-08-02 14:51:37 +0530

    Praveen (#5)
    * Update Main_Home.jsx
    
    * basic design with routing

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Importing CSS for styling
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <h2><Link to="/">DSP HOME RENTAL LTD</Link> </h2>
+      <div className="nav-links">
+        {/* <Link to="/home">Home</Link> */}
+        <Link to="/buy">Buy</Link>
+        <Link to="/rent">Rent</Link>
+        <Link to="/lease">Lease</Link>
+        <Link to="/Enqiry">Enqiry</Link>
+      </div>          
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
