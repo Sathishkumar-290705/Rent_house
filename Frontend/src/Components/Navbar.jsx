7369850a0e0b6e4bfaa969011102cd0e6230445a
SATHISH KUMAR <sathishkumar290705@gmail.com>
2025-08-05 11:40:58 +0530

    nav bar fixed
    about us created

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Importing CSS for styling
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h2><Link to="/">DSP HOME RENTAL LTD</Link> </h2>
-      <div className="nav-links">
-        {/* <Link to="/home">Home</Link> */}
-        <Link to="/buy">Buy</Link>
-        <Link to="/rent">Rent</Link>
-        <Link to="/lease">Lease</Link>
-        <Link to="/Enqiry">Enqiry</Link>
-      </div>          
+      <h2><Link to="/">DSP HOME RENTAL LTD</Link></h2>
+
+      <di v className="nav-links">
+
+      <div>
+
+          <Link to="/about-us">About Us</Link>
+      </div>
+
+        <div className="nav-item">
+        
+          <Link>Services</Link>
+          <div className="submenu">
+            <Link to="/buy">Buy</Link>
+            <Link to="/rent">Rent</Link>
+            <Link to="/lease">Lease</Link>
+          </div>
+        </div>
+
+        <div className="nav-item">
+          <Link to="/enquiry">Enquiry</Link>
+        </div>
+
+      </di>
     </nav>
   );
 };
